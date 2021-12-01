6e1aeb0eca3e1eafd4d7c47bbc8a7e0de351c4b7
Bank <elroby444@gmail.com>
2021-12-01 09:59:41 +0100

    Añadidos mas test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -15,21 +15,27 @@ public class AppTest
     @Test
     void sum() {
         assertEquals(2,App.sum(1,1));
+        assertEquals(-1,App.sum(-2,1));
     }
 
     @Test
     void sub() {
         assertEquals(0,App.sub(1,1));
-
-        assertEquals(1,App.sub(2,1));
+        assertEquals(-1,App.sub(1,2));
     }
 
     @Test
     void div() {
+        assertEquals(1,App.div(2,2));
+        assertEquals(3,App.div(6,2));
+        assertEquals(5,App.div(11,2));
+        assertEquals(0,App.div(5,0));
     }
 
     @Test
     void mul() {
+        assertEquals(4,App.mul(2,2));
+        assertEquals(-2,App.mul(-1,2));
 
     }
 }
