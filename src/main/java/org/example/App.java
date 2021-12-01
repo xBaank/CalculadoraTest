a33324d97745e795e013a272aab7edcf301b4ea8
Bank <elroby444@gmail.com>
2021-12-01 09:13:06 +0100

    added methods

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,4 +10,17 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    
+    public static int sum(int num1,int num2) {
+
+    }
+    public static int sub(int num1,int num2) {
+
+    }
+    public static int div(int num1,int num2) {
+
+    }
+    public static int mul(int num1,int num2) {
+
+    }
 }
