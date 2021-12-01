14a5dbb1a3168f872d48d106116cef02f6272c94
Bank <elroby444@gmail.com>
2021-12-01 09:43:16 +0100

    test added

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,8 +1,7 @@
 package org.example;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
@@ -12,9 +11,25 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void sum() {
+        assertEquals(2,App.sum(1,1));
+    }
+
+    @Test
+    void sub() {
+        assertEquals(0,App.sub(1,1));
+
+        assertEquals(1,App.sub(2,1));
+    }
+
+    @Test
+    void div() {
+    }
+
+    @Test
+    void mul() {
+
     }
 }
