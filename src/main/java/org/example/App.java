90fbadd526ccc78e8e3b1b89d687d290f8445e75
Xavier <hectormarin2000@gmail.com>
2021-12-01 09:58:53 +0100

    CommitHX02

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+
     }
     
     public static int sum(int num1,int num2) {
@@ -20,8 +20,13 @@ public class App
     return rest;
     }
     public static int div(int num1,int num2) {
-    int coc = num1/num2;
-    return coc;
+        int coc;
+        if (num2 == 0) {
+            coc = Integer.MIN_VALUE;
+        }else {
+            coc = num1 / num2;
+        }
+        return coc;
     }
     public static int mul(int num1,int num2) {
     int res = num1 * num2;
